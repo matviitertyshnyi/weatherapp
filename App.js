cc0658e258d9e8cbc87039a876a99bec7c53d00d
matvii <vladtratata10@gmail.com>
2024-02-21 13:37:38 +0100

    Map Added

--- App.js
+++ App.js
@@ -1,20 +1,75 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from "react";
+import {
+  StyleSheet,
+  View,
+  TouchableOpacity,
+  Text,
+  Dimensions,
+  Image,
+} from "react-native";
+import * as Location from "expo-location";
+import MapView, { Marker } from "react-native-maps";
 
-export default function App() {
+
+const Waiting_Driver_Screen = () => {
+  const [currentLocation, setCurrentLocation] = useState(null);
+  const [initialRegion, setInitialRegion] = useState(null);
+
+  useEffect(() => {
+    const getLocation = async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        console.log("Permission to access location was denied");
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setCurrentLocation(location.coords);
+
+      setInitialRegion({
+        latitude: location.coords.latitude,
+        longitude: location.coords.longitude,
+        latitudeDelta: 0.005,
+        longitudeDelta: 0.005,
+      });
+    };
+
+    getLocation();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Weather</Text>
-      <StatusBar style="auto" />
+      {initialRegion && (
+        <MapView style={styles.map} initialRegion={initialRegion}>
+          {currentLocation && (
+            <Marker
+              coordinate={{
+                latitude: currentLocation.latitude,
+                longitude: currentLocation.longitude,
+              }}
+              title="Your Location"
+            />
+          )}
+        </MapView>
+      )}
+      {/* Rest of your code */}
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  map: {
+    position: "absolute",
+    top:0,
+    left:0,
+    width: "100%",
+    height: "25%",
   },
 });
+
+export default Waiting_Driver_Screen;
